f56b5bcb182f449151094b0cc9bd2fe018da6d90
neo1go <neo1go@gmx.de>
2025-05-08 18:11:06 +0200

    Design Patterns hinzugefügt

---
+++ Design Patterns/StrategyPattern/StrategyPattern/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace StrategyPattern
+{
+
+    //Basierend auf dem Konstruktoraufruf in der Main-Methode wird
+    //mittels Polymorphie der Interface-Methode transport() die passende Funktion gewählt.
+
+    public interface ITransportStrategy
+    {
+        string transport();
+
+    }
+
+    public class WalkStrategy : ITransportStrategy //Dies sind Klassen, die polymorph die Methode zurückgeben
+    {
+        public string transport()
+        {
+            return "Walking to work.";
+
+        }
+    }
+
+    public class CarStrategy : ITransportStrategy //Dies sind Klassen, die polymorph die Methode zurückgeben
+    {
+        public string transport()
+        {
+            return "Driving to work with my car.";
+
+        }
+    }
+
+    public class BusStrategy : ITransportStrategy //Dies sind Klassen, die polymorph die Methode zurückgeben
+    {
+        public string transport()
+        {
+            return "Using the bus to get to work.";
+
+        }
+    }
+
+    public class BikeStrategy : ITransportStrategy //Dies sind Klassen, die polymorph die Methode zurückgeben
+    {
+        public string transport()
+        {
+            return "Using the bike to drive to work";
+
+        }
+    }
+    public class BetterCommuter()
+    {
+        private ITransportStrategy? _strategy;   //Privates Feld für Komposition, also die DP
+        // Das Interface wird nicht vererbt sondern es wird eine Variable über eine Setter Methode bereitgestellt
+
+        public void SetStrategy(ITransportStrategy strategy)//hiermit wird die tatsächliche Strategie initialisiert obwohl
+                                                            //dies kein Konstruktor ist, sondern eine Setter-Methode.
+        {
+            _strategy = strategy;
+        }
+
+        public void GoToWork()
+        {
+            if (_strategy != null)
+            {
+                Console.WriteLine(_strategy.transport()); // WICHTIG!!!!
+                                                          // hier wird dann die passende Strategie, polymorph
+                                                          // je nach Objekterzeugung durch seperate Klassen, eingefügt.
+            }
+            else
+            {
+                Console.WriteLine("No transport strategy has been set.");
+            }
+        }
+
+    }
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var commuter = new BetterCommuter();
+
+
+            var walking = new WalkStrategy();
+            commuter.GoToWork();  //hier ist der Setter noch nicht ausgeführt worden
+            commuter.SetStrategy(walking);        //Setter Aufruf
+            commuter.GoToWork();                  //Je nach erstelltem Objekt wird eine Variante von GoToWork() aufgerufen.
+
+
+            var driving = new CarStrategy();
+            commuter.SetStrategy(driving);
+            commuter.GoToWork();
+
+
+            var busComute = new BusStrategy();
+            commuter.SetStrategy(busComute);
+            commuter.GoToWork();
+        }
+    }
+}
---
+++ Design Patterns/StrategyPatternII/StrategyPatternII/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace StrategyPatternII
+{
+    public interface IFLy
+    {
+        public string Fly();
+    }
+
+    public abstract class Animal
+    {
+        public IFLy? FlyingType { get; set; } //Instanz-Eigenschaft   
+       
+
+        public Animal(IFLy flyingType)  //dient hier nur dem Interface
+        {
+            FlyingType = flyingType; //Das Objekt FlyingType ist eine Schnittstelle (Interface-Referenz)
+        }
+
+        public string TryToFly() //Dies ist ein einheitlicher ,polymorpher Zugriffspunkt auf die Fähigkeit zu fliegen.
+                                 //Somit wird die Fluglogik CanFly,CantFly von der Tierlogik Animal getrennt
+        {
+            return FlyingType!.Fly();    //Hier wird die Interfacemethode an das Objekt gekoppelt              (NonNullable -> !)
+        }
+
+        public void SetFlyingAbility(IFLy newFlyType) //Setter, der die Methode bei new-Operator neu setzt.
+        {
+            FlyingType = newFlyType;
+        }
+    }
+
+    public class CanFly : IFLy   //Dies sind die Klassen, die dann die realen Methoden an die vererbende Klassen weitergeben.
+    {
+        public string Fly()
+        {
+            return "Ich kann fliegen.";
+        }
+    }
+
+    public class CantFly : IFLy    //Dies sind die Klassen, die dann die realen Methoden an die vererbende Klassen weitergeben.
+    {
+        public string Fly()
+        {
+            return "Ich kann nicht fliegen.";
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public Dog() : base (new CantFly())   //hier wird das Interface im Konstruktor sofort übergeben.
+        {
+
+        }
+    }
+    public class Bird : Animal
+    {
+        public Bird():base (new CanFly())  //hier wird das Interface im Konstruktor sofort übergeben.
+        {
+            
+        }
+    }
+       
+
+    public class Program
+    {
+        public static void Main(String[] args) 
+        {
+            Bird birdy = new Bird();
+            Console.WriteLine($"Vogel {birdy.TryToFly()} ");
+
+
+            Animal sparky = new Dog();         
+            Console.WriteLine($"Hund {sparky.TryToFly()}");
+
+
+            sparky.SetFlyingAbility(new CanFly()); //Hier wird mittels Setter strategisch die Eigenschaft möglich gemacht.
+            Console.WriteLine($"Hund nach Änderung {sparky.TryToFly()}");
+            
+        }
+    }
+}
