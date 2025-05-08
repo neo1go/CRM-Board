441930d1a165c0470ac92ac68caf05801f5cca78
neo1go <neo1go@gmx.de>
2025-05-08 10:43:23 +0200

    Leetcodeaufgaben dem remote Repo hinzugefügt

---
+++ LeetcodeSolutions/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace BinarySearch
+{
+
+    public class Program
+    {
+        public static bool ReturnTrueIfValueIsinArray(int[] values, int target)
+        {
+            int linkerPtr = 0;
+            int rechterPtr = values.Length - 1;
+            while (linkerPtr <= rechterPtr)
+            {
+                //Berechnung des mittleren Index
+                int middle = (linkerPtr + rechterPtr) / 2; 
+
+                //Prüfen,ob Zielwert im rechten Bereich liegt
+                if (target > values[middle])    
+                {
+
+                    linkerPtr = middle + 1;  
+
+                }
+                //Prüfen,ob Zielwert im linken Bereich liegt
+                else if (target < values[middle])    //else if ist wichtig, um genau diese beiden Optionen auch abzugleichen.
+                {
+                    rechterPtr = middle - 1;
+
+                }
+                //Zielwert gefunden
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static void Main()
+        {
+            int[] myValues = [ 1, 3, 5, 6, 7, 8, 9, 11 ]; //muss sortiert sein, WICHTIG
+            int target = 3;
+
+            Console.WriteLine("Die Zahl ist im Array vorhanden: " + ReturnTrueIfValueIsinArray(myValues, target));
+        }
+
+
+    }
+}
---
+++ LeetcodeSolutions/findmissingandrepeatedvalues/FindMissingAndRepeatedValues/Program.cs
@@ -0,0 +1,64 @@
+﻿// Leetcode 2965
+// Finden von Duplikat und fehlender Zahl in einer Matrix.
+// Es werden die Werte in ein HashSet eingefügt. Bei Erreichen eines Duplikates wird
+// dieses sofort in die Lösungsmenge eingetragen.
+// Bei dem HashSet mit den zu erwartenden Nummern werden alle besuchten Zahlen gelöscht und
+// es bleibt nur der fehlende Eintrag zurück als einziger Eintrag.
+
+namespace FindMissingAndRepeatedValues
+{
+
+
+    public class Program
+    {
+        private int[] FindMissingAndRepeatedValues(int[][] grid)
+        {
+            int n = grid.Length;
+            HashSet<int>
+                numbers = new(Enumerable.Range(1,
+                    n * n)); //Erstellt ein HashSet mit den Werten von 1 bis n
+            int duplicate = -1;
+
+
+            //  Gitter durchgehen und Werte zählen
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    int value = grid[i][j];
+
+                    if (!numbers.Contains(value)) //ein mögliches Duplikat wird beim Iterieren sofort erkannt
+                                                  // da ja alle besuchten Werte immer entfernt werden.
+                                                  // Da ein Duplikat einem doppelten Besuch gleichkommt und der Wert schon aus dem
+                                                  // HashSet entfernt wurde, wird dieser nicht vorhandene Wert als Duplikat erkannt.
+                    {
+                        duplicate = value;
+                    }
+                    else
+                    {
+                        numbers.Remove(value); // Gefundene Zahlen werden einfach entfernt.
+                    }
+                }
+            }
+
+            //  Die einzige verbleibende Zahl im Set ist die fehlende Zahl
+            int missing = numbers.First();
+
+            return [duplicate, missing];
+        }
+
+        public static void Main()
+        {
+            int[][] grid =
+            [
+                [1, 3],
+                [2, 2] // "2" kommt doppelt vor, "4" fehlt
+            ];
+
+            Program program = new Program();
+            var ergebnis = program.FindMissingAndRepeatedValues(grid);
+            Console.WriteLine($"Das Ergebnis ist: [{string.Join(", ", ergebnis)}]");
+        }
+    }
+
+}
